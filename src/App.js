04deff3f80b6a380746a7a930668b9fbbf430030
unknown <rockydasgfx@gmail.com>
2022-06-22 14:00:57 +0600

    firebase setup and show singin user data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import app from "./firebase.init";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const googleSingin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FireBase auth</h1>
+      <button onClick={googleSingin}>Google Singin</button>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUDML7wS5jDixd4wcC8fxr9CL2f4myfhk",
+  authDomain: "simple-react-firebase-au-ba8d3.firebaseapp.com",
+  projectId: "simple-react-firebase-au-ba8d3",
+  storageBucket: "simple-react-firebase-au-ba8d3.appspot.com",
+  messagingSenderId: "767983559531",
+  appId: "1:767983559531:web:f4e638960826e228a6e22e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
